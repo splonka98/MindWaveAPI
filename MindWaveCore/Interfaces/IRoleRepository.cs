e1ed0a72faf77c7632898c2d32d5378081ce39d4
splonka98 <szymon.plonka@microsoft.wsei.edu.pl>
2024-12-08 14:55:04 +0100

    Add interfaces to domain

---
+++ MindWaveCore/Interfaces/IRoleRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindWaveCore.Interfaces
+{
+    internal interface IRoleRepository
+    {
+    }
+}
---
+++ MindWaveCore/Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using MindWaveCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindWaveCore.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserAsync(int id);
+        Task<User> GetUserAsync(string email, string password);
+        Task CreateUserAsync(User user, string password);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(int id);
+    }
+}
