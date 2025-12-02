a93398ce60353aa64a99b1905618a7e225796138
WPGT37@wpgt37-z15g8 <szymon.plonka@motorolasolutions.com>
2025-12-02 17:34:09 +0100

    Add Domain Surveys

---
+++ Domain/Surveys/SurveyAnswer.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Surveys;
+
+public sealed class SurveyAnswer
+{
+    public int QuestionId { get; private set; }
+    public string Value { get; private set; } = string.Empty;
+
+    private SurveyAnswer() { }
+
+    public SurveyAnswer(int questionId, string value)
+    {
+        QuestionId = questionId;
+        Value = value;
+    }
+}
---
+++ Domain/Surveys/SurveyInstance.cs
@@ -0,0 +1,29 @@
+﻿namespace Domain.Surveys;
+
+public sealed class SurveyInstance
+{
+    public Guid Id { get; private set; }
+    public Guid PatientUserId { get; private set; }
+    public Guid TemplateId { get; private set; }
+    public DateOnly Date { get; private set; }
+    public List<SurveyAnswer> Answers { get; private set; } = new();
+    public List<string> Tags { get; private set; } = new(); // Ignored in EF mapping currently
+
+    private SurveyInstance() { }
+
+    public static SurveyInstance Create(Guid id, Guid patientUserId, Guid templateId, DateOnly date)
+    {
+        return new SurveyInstance
+        {
+            Id = id,
+            PatientUserId = patientUserId,
+            TemplateId = templateId,
+            Date = date
+        };
+    }
+
+    public void AddAnswer(int questionId, string value)
+    {
+        Answers.Add(new SurveyAnswer(questionId, value));
+    }
+}
