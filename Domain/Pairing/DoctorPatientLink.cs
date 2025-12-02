38bce4e3a8c247e256b659d5a07c30f7f4cab806
WPGT37@wpgt37-z15g8 <szymon.plonka@motorolasolutions.com>
2025-12-02 17:33:41 +0100

    Add Domain Pairing

---
+++ Domain/Pairing/DoctorPatientLink.cs
@@ -0,0 +1,22 @@
+﻿namespace Domain.Pairing;
+
+public sealed class DoctorPatientLink
+{
+    public Guid Id { get; private set; }
+    public Guid DoctorUserId { get; private set; }
+    public Guid PatientUserId { get; private set; }
+    public DateTime LinkedAtUtc { get; private set; }
+
+    private DoctorPatientLink() { }
+
+    public static DoctorPatientLink Create(Guid id, Guid doctorUserId, Guid patientUserId, DateTime linkedAtUtc)
+    {
+        return new DoctorPatientLink
+        {
+            Id = id,
+            DoctorUserId = doctorUserId,
+            PatientUserId = patientUserId,
+            LinkedAtUtc = linkedAtUtc
+        };
+    }
+}
---
+++ Domain/Pairing/PairingToken.cs
@@ -0,0 +1,32 @@
+﻿namespace Domain.Pairing;
+
+public sealed class PairingToken
+{
+    public Guid Id { get; private set; }
+    public Guid PatientUserId { get; private set; }
+    public string Code { get; private set; } = string.Empty;
+    public DateTime ExpiresAtUtc { get; private set; }
+    public bool Redeemed { get; private set; }
+
+    private PairingToken() { }
+
+    public static PairingToken Issue(Guid id, Guid patientUserId, string code, DateTime expiresAtUtc)
+    {
+        return new PairingToken
+        {
+            Id = id,
+            PatientUserId = patientUserId,
+            Code = code,
+            ExpiresAtUtc = expiresAtUtc,
+            Redeemed = false
+        };
+    }
+
+    public bool CanRedeem(DateTime nowUtc) => !Redeemed && nowUtc <= ExpiresAtUtc;
+
+    public void MarkRedeemed()
+    {
+        if (Redeemed) return;
+        Redeemed = true;
+    }
+}
