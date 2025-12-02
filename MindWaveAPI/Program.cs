540917ac4e3166baa18da9a5c8321337e662b709
WPGT37@wpgt37-z15g8 <szymon.plonka@motorolasolutions.com>
2025-12-02 17:39:59 +0100

    update builder in Program.cs

--- MindWaveAPI/Program.cs
+++ MindWaveAPI/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<Infrastructure.Persistence.MindWaveDbContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddScoped<Application.Abstractions.Auth.ILoginService, Infrastructure.Auth.LoginService>();
+builder.Services.AddScoped<Application.Abstractions.Auth.IRegistrationService, Infrastructure.Auth.RegistrationService>();
 
 var app = builder.Build();
 
