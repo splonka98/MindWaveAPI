e8d1ce37634761b04cb281c45523755fb37b0189
WPGT37@wpgt37-z15g8 <szymon.plonka@motorolasolutions.com>
2025-12-02 17:31:42 +0100

    Change Login Response to simplify

--- Application/Contracts/Auth/LoginResponse.cs
+++ Application/Contracts/Auth/LoginResponse.cs
@@ -2,7 +2,7 @@ namespace Application.Contracts.Auth;
 
 public sealed class LoginResponse
 {
-    public string AccessToken { get; init; } = string.Empty;
-    public string RefreshToken { get; init; } = string.Empty;
-    public DateTime ExpiresAtUtc { get; init; }
+    public Guid UserId { get; init; }
+    public string Email { get; init; } = string.Empty;
+    public string Role { get; init; } = string.Empty;
 }
