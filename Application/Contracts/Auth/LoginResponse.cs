23eaba002bb409eac8eb6a9a6de7bb3bfd205301
WPGT37@wpgt37-z15g8 <szymon.plonka@motorolasolutions.com>
2025-12-07 10:40:15 +0100

    add JWT token to auth contract loginResponse:

--- Application/Contracts/Auth/LoginResponse.cs
+++ Application/Contracts/Auth/LoginResponse.cs
@@ -2,7 +2,8 @@ namespace Application.Contracts.Auth;
 
 public sealed class LoginResponse
 {
-    public Guid UserId { get; init; }
-    public string Email { get; init; } = string.Empty;
-    public string Role { get; init; } = string.Empty;
+    public Guid UserId { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
 }
