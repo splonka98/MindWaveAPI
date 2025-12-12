d368fb085188051258765a0de42ff165e57f75eb
WPGT37@wpgt37-z15g8 <szymon.plonka@motorolasolutions.com>
2025-12-12 23:51:38 +0100

    cleaning git folder

--- Application.Contracts/Auth/LoginRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace Application.Contracts.Auth;
-
-public sealed class LoginRequest
-{
-    public string Email { get; init; } = string.Empty;
-    public string Password { get; init; } = string.Empty;
-}
